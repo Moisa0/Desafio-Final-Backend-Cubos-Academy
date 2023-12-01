d8eef5bcbb930b81b869158d3a635d7ced0ed9c1
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-01 01:23:16 -0300

    Inserindo importação de intermediario e schema no rotas usuario

--- src/rotas/usuario.js
+++ src/rotas/usuario.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import * as u from '../controladores/usuario.js'
+import { validarCorpoRequisicao } from '../intermediarios/validacaoUsuario.js'
+import { schemaUsuario } from '../schema/usuario.js'
 
 export const rotasUsuario = Router()
 
 rotasUsuario.route('/usuario')
-    .post(u.cadastrar)
+    .post(validarCorpoRequisicao(schemaUsuario), u.cadastrar)
