ac1578998af03318a60c9d0385ab53e01a29433f
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-01 01:18:42 -0300

    Importação do dotenv

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 
 export const app = express()
 
 const porta = process.env.PORT || 3000
 
-app.listen(porta)
+app.listen(porta, () => console.log("Servidor ON"))
