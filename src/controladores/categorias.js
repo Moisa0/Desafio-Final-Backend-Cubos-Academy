77261583b330a13d0e6c2502b39d7dbcbcbde3fc
Rafael Alves <faelalves957@gmail.com>
2023-12-02 20:21:10 -0300

    Implementação do 'Endpoint' listar categorias

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -1,3 +1,14 @@
-export const listar = (req, res) => {
+import { knex } from '../conexao/conexao.js'
 
+export async function listar (req, res) {
+    try {
+
+        const categorias = await knex("categorias")
+    return res.status(200).json(categorias);
+
+    } catch (error) {
+        //console.log (error.mensage)
+        return res.status(500).json({ mensagem: "Erro interno do servidor!"})      
+    }
+    
 }
