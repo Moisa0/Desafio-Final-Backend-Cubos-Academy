b41381144a0b0f41dd8db7d0e8ef58017db253ad
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-02 21:50:48 -0300

    Rota para login

--- src/rotas/login.js
+++ src/rotas/login.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import * as l from '../controladores/login.js'
+import { schemaLogin } from '../schema/login.js'
+import { validarCorpoLogin } from '../intermediarios/validacaoLogin.js'
 
 export const rotasLogin = Router()
 
 rotasLogin.route('/login')
-    .post(l.logar)
+    .post(validarCorpoLogin(schemaLogin), l.logar)
