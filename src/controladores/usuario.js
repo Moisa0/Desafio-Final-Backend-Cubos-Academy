7cc30480b3ff3c526ac1a58dfb32e55753f6f23d
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-04 21:27:31 -0300

    Adicionando função atualizar usuario

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -29,3 +29,33 @@ export const cadastrar = async (req, res) => {
         return mensagemJson(500, res, error.message)
     }
 }
+
+export const atualizar = async (req, res) => {
+    const { nome, email, senha } = req.body
+
+    try {
+        const verificandoEmail = await knex("usuarios").where({ email })
+        
+        if(verificandoEmail.length !== 0 && verificandoEmail[0].email !== req.usuario.email) {
+            return mensagemJson(400, res, "O email fornecido já está cadastrado.")
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+        const usuario = {
+            nome,
+            email,
+            senha: senhaCriptografada
+        }
+        
+        const { id } = req.usuario
+
+        const atualizarUsuario = await knex("usuarios").update(usuario).where({ id }).returning([ "id", "nome", "email" ])
+
+        return mensagemJson(200, res, atualizarUsuario)
+    } catch (error) {
+        console.log(error);
+        return mensagemJson(500, res, error.message)
+        
+    }
+}
