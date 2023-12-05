daa3acc658078f88c1a884bc666a44a4af4d0f17
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-04 21:26:31 -0300

    Rota para atualizar usuario

--- src/rotas/usuario.js
+++ src/rotas/usuario.js
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import * as u from '../controladores/usuario.js'
 import { validarCorpoRequisicao } from '../intermediarios/validacaoUsuario.js'
 import { schemaUsuario } from '../schema/usuario.js'
+import { autenticarToken } from '../intermediarios/autenticacaoToken.js'
 
 export const rotasUsuario = Router()
 
 rotasUsuario.route('/usuario')
     .post(validarCorpoRequisicao(schemaUsuario), u.cadastrar)
+    .put(validarCorpoRequisicao(schemaUsuario), autenticarToken, u.atualizar)
