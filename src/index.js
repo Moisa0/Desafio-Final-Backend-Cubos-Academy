691853f9eee105663d882b7c33b44ff1fadc575c
Felipe Guimarães <felipe.aguimaraes10@gmail.com>
2023-12-05 02:01:53 -0300

    correção de bug no arquivo server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import express from 'express'
-import { app } from './server.js'
 import { todasRotas } from './rotas/export/exportRotas.js'
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log('oi')
+const app = express()
+const porta = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.use(todasRotas)
+
+app.listen(porta, () => console.log("Servidor ON"))
