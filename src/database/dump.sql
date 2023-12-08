3069f922902ef6196184a3cbdf6bbc3072afd770
Felipe Guimarães <felipe.aguimaraes10@gmail.com>
2023-12-08 00:25:59 -0300

    tabela cliente e produto criados no dump.js

--- src/database/dump.sql
+++ src/database/dump.sql
@@ -1,15 +1,38 @@
+DROP DATABASE IF EXISTS pdv;
+
 CREATE DATABASE pdv;
 
-CREATE TABLE usuarios(
+CREATE TABLE usuarios (
   id serial PRIMARY KEY,
   nome text NOT NULL,
   email text NOT NULL UNIQUE,
   senha text NOT NULL
 );
 
-CREATE TABLE categorias(
+CREATE TABLE categorias (
 	id serial PRIMARY KEY,
   descricao text NOT NULL UNIQUE
+);
+
+CREATE TABLE produtos (
+  id serial PRIMARY KEY,
+  descricao text NOT NULL,
+  quantidade_estoque int NOT NULL,
+  valor int NOT NULL,
+  categoria_id int REFERENCES categorias(id)
+);
+
+CREATE TABLE clientes (
+  id serial PRIMARY KEY
+  nome text NOT NULL,
+  email text NOT NULL UNIQUE,
+  cpf text NOT NULL UNIQUE,
+  cep text NOT NULL,
+  rua text NOT NULL,
+  numero int NOT NULL,
+  bairro text NOT NULL,
+  cidade text NOT NULL,
+  estado text NOT NULL
 );
 
 INSERT INTO categorias (descricao) 
