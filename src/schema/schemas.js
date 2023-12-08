20f7080c9b5d9e278e2dd0bd9f29fa1dc937b0db
Felipe Guimarães <felipe.aguimaraes10@gmail.com>
2023-12-08 03:34:35 -0300

    Novos schemas adicionados

--- src/schema/schemas.js
+++ src/schema/schemas.js
@@ -1,22 +1,51 @@
-import joi from 'joi'
+import Joi from 'joi'
 
-const email = joi.string().email().trim().required().messages({
-    'string.trim': 'Não é permitido espaços no começo ou no final.',
-    'any.required': 'O campo email é obrigatório',
+const email = Joi.string().email().max(30).required().messages({
+    'string.max': 'O email precisa ter no máximo 30 caracteres',
+    'any.required': 'O campo "email" é obrigatório',
     'string.email': 'Insira um email valido',
-    'string.empty': 'O campo email não poder estar vazio'
+    'string.empty': 'O campo "email" não poder estar vazio'
 })
 
-const senha = joi.string().required().messages({
-    'any.required': 'O campo senha é obrigatório',
-    'string.empty': 'O campo senha não poder estar vazio'
+const senha = Joi.string().min(8).max(72).required().messages({
+    'string.max': 'A senha precisa ter no máximo 72 caracteres',
+    'string.min': 'A senha precisa ter no mínino 8 caracteres',
+    'any.required': 'O campo "senha" é obrigatório',
+    'string.empty': 'O campo "senha" não poder estar vazio'
 })
 
-const nome = joi.string().required().messages({
-    'any.required': 'O campo nome é obrigatório',
-    'string.empty': 'O campo nome não poder estar vazio'
+const nome = Joi.string().max(30).required().messages({
+    'string.max': 'O nome precisa ter no máximo 30 caracteres',
+    'any.required': 'O campo "nome" é obrigatório',
+    'string.empty': 'O campo "nome" não poder estar vazio'
 })
 
-export const login = joi.object({email, senha})
 
-export const usuario = joi.object({nome, email, senha})
+const descricao = Joi.string().required().messages({
+    'any.required': 'O campo "descricao" é obrigatório',
+})
+
+const quantidade_estoque = Joi.number().integer().positive().required().vali.messages({
+    'number.integer': 'Informe um número inteiro',
+    'number.positive': 'Informe um número positivo',
+    'number.base': 'Informe um número valido',
+    'any.required': 'O campo "quantidade_estoque" é obrigatório',
+})
+
+const valor = Joi.number().integer().positive().required().messages({
+    'number.integer': 'Informe um número inteiro',
+    'number.positive': 'Informe um número positivo',
+    'number.base': 'Informe um número valido',
+    'any.required': 'O campo "valor" é obrigatório',
+})
+
+const categoria_id = Joi.number().integer().positive().required().messages({
+    'any.required': 'O campo "categoria_id" é obrigatório',
+})
+
+
+export const login = Joi.object({email, senha})
+
+export const usuario = Joi.object({nome, email, senha})
+
+export const produto = Joi.object({descricao, quantidade_estoque, valor, categoria_id})
