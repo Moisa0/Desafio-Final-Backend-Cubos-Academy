37977048b18f3292348adaf5eba7af05053bfdc5
moisa <telesmoises8@gmail.com>
2023-12-11 17:16:20 -0300

    criação do controlador de excluir usuario. Pendente a testes ainda

--- src/controladores/produto.js
+++ src/controladores/produto.js
@@ -23,9 +23,29 @@ export const editar = (req, res) => {
 }
 
 export const detalhar = (req, res) => {
+    
 
 }
 
-export const excluir = (req, res) => {
-    
+export const excluir = async (req, res) => {
+ 
+        const { id } = req.params; 
+     
+        try {
+     
+            const produto = await knex('produtos').where({ id }).first();
+     
+            if (!produto) {
+                return res.status(404).json({ mensagem: 'Produto não encontrado' });
+            }
+     
+     
+            await knex('produtos').where({ id }).del();
+     
+            return res.status(200).json({ mensagem: 'Produto excluído com sucesso' });
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).json({mensagem: `Erro interno do servidor: ${message.error}`});
+        }
+
 }
